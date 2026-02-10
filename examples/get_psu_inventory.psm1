af8919ce771278d2e4b134e410f4bf976f65cfb0
mark <marktsai0316@gmail.com>
2026-02-10 10:31:29 +0800

    Update get_psu_inventory.psm1

--- examples/get_psu_inventory.psm1
+++ examples/get_psu_inventory.psm1
@@ -102,7 +102,7 @@ function get_psu_inventory
             # Get system resource
             $url_address_system = "https://$ip"+$system_url_string
             $response = Invoke-WebRequest -Uri $url_address_system -Headers $JsonHeader -Method Get -UseBasicParsing    
-            $converted_object = $response.Content | ConvertFrom-Json
+            $converted_object = $response.Content | ConvertFrom-JsonWithDuplicates #ConvertFrom-Json : fixed ASUS Bug: Unable to convert the JSON string because the dictionary created from this string contains duplicate keys 'AMI' and 'Ami'.
             
             # Get chassis resource 
             $chassis_url = "https://$ip" + $converted_object.Links.Chassis."@odata.id"  
