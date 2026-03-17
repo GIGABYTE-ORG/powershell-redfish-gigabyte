c087618e7db606b9c2afb2749c97a51c487acde8
mark <marktsai0316@gmail.com>
2026-03-17 21:28:24 +0800

    Update get_server_boot_once_types.psm1

--- examples/get_server_boot_once_types.psm1
+++ examples/get_server_boot_once_types.psm1
@@ -135,6 +135,7 @@ function get_server_boot_once_types
 #>                    
             
             # Get bios boot once information
+<#
             $boot_once_dict["BootSourceOverrideEnabled@Redfish.AllowableValues"] =  $converted_object."Boot"."BootSourceOverrideEnabled@Redfish.AllowableValues"
             if($boot_once_dict["BootSourceOverrideEnabled@Redfish.AllowableValues"] -eq $null )
             {  
@@ -152,6 +153,7 @@ function get_server_boot_once_types
                             "Legacy",
                             "UEFI")
             }
+#>            
             $boot_once_dict["BootSourceOverrideTarget@Redfish.AllowableValues"] =  $converted_object."Boot"."BootSourceOverrideTarget@Redfish.AllowableValues"
             if($boot_once_dict["BootSourceOverrideTarget@Redfish.AllowableValues"] -eq $null )
             {  
