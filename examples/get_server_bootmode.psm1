0e62fc66d5e6848577cae5a0a7ca6f342833bfcd
mark <marktsai0316@gmail.com>
2026-03-18 13:08:49 +0800

    Rename function get_bios_bootmode to get_server_bootmode

--- examples/get_server_bootmode.psm1
+++ examples/get_server_bootmode.psm1
@@ -24,7 +24,7 @@
 ###get
 Import-module $PSScriptRoot\lenovo_utils.psm1
 
-function get_bios_bootmode
+function get_server_bootmode
 {
     <#
    .Synopsis
@@ -37,7 +37,7 @@ function get_bios_bootmode
     - system_id:Pass in ComputerSystem instance id(None: first instance, all: all instances)
     - config_file: Pass in configuration file path, default configuration file is config.ini
    .EXAMPLE
-    get_bios_bootmode -ip 10.10.10.10 -username USERID -password PASSW0RD 
+    get_server_bootmode -ip 10.10.10.10 -username USERID -password PASSW0RD 
    #>
    
     param(
